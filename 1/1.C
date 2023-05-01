c26d2d5c7087b071ce5af6133fc6bfd1e143c011
HetSavani36 <hetsavani36@gmail.com>
2023-05-01 16:38:13 +0530

    day-1

---
+++ 1/1.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+clrscr();
+printf("\t--------\n");
+printf("\t|      |\n");
+printf("\tH      |\n");
+printf("\tE      |\n");
+printf("\tT      |\n");
+printf("\t|      |\n");
+printf("\t--------\n");
+
+
+
+getch();
+
+}
