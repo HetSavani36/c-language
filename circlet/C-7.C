71e370ed0cd4401b87d2279aedbad1920f45a8a9
HetSavani36 <hetsavani36@gmail.com>
2023-05-25 16:51:19 +0530

    exam

---
+++ circlet/C-7.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,j;
+ clrscr();
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+
+			if(i==1)
+			{
+				printf("* ");
+			}
+			else if(i==2 || j==1 || j==5)
+			{
+				printf("* ");
+			}
+		}
+		printf("\n");
+	}
+ getch();
+}
